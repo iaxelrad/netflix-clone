37eb694352ec753cfc1ff20c967e80fb68e87d9e
Itamar Axelrad <iaxelrad@gmail.com>
2021-04-03 10:57:13 +0300

    Added initial pages to app.js

--- src/App.js
+++ src/App.js
@@ -7,6 +7,15 @@ import { Home, Browse, Signin, Signup } from './pages';
 export default function App() {
   return (
     <Router>
+      <Route exact path={ROUTES.BROWSE}>
+        <Browse />
+      </Route>
+      <Route exact path={ROUTES.SIGN_IN}>
+        <Signin />
+      </Route>
+      <Route exact path={ROUTES.SIGN_UP}>
+        <Signup />
+      </Route>
       <Route exact path={ROUTES.HOME}>
         <Home />
       </Route>
