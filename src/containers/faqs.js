086fb58a0a357b601be832b9585f4f081ee5e06f
Itamar Axelrad <iaxelrad@gmail.com>
2021-04-03 10:27:14 +0300

    Added the OptForm component.

--- src/containers/faqs.js
+++ src/containers/faqs.js
@@ -1,5 +1,5 @@
 import faqsData from '../fixtures/faqs.json';
-import { Accordion } from '../components';
+import { Accordion, OptForm } from '../components';
 
 export function FaqsContainer() {
   return (
@@ -11,6 +11,15 @@ export function FaqsContainer() {
           <Accordion.Body>{item.body}</Accordion.Body>
         </Accordion.Item>
       ))}
+
+      <OptForm>
+        <OptForm.Input name="email" type="email" placeholder="Email address" />
+        <OptForm.Button>Get started</OptForm.Button>
+        <OptForm.Break />
+        <OptForm.Text>
+          Ready to watch? Enter your email to create or restart your membership.
+        </OptForm.Text>
+      </OptForm>
     </Accordion>
   );
 }
