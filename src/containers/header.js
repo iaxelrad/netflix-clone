1dd12a60928412cd71d1a674bad961366f7db29b
Itamar Axelrad <iaxelrad@gmail.com>
2021-04-05 23:51:34 +0300

    fix for the header.

--- src/containers/header.js
+++ src/containers/header.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Header } from '../components';
 import logo from '../logo.svg';
+import * as ROUTES from '../constants/routes';
 
 export function HeaderContainer({ children }) {
   return (
     <Header>
       <Header.Frame>
-        <Header.Logo src={logo} />
-        <Header.ButtonLink>Sign in</Header.ButtonLink>
+        <Header.Logo to={ROUTES.HOME} src={logo} />
+        <Header.ButtonLink to={ROUTES.SIGN_IN}>Sign in</Header.ButtonLink>
       </Header.Frame>
       {children}
     </Header>
