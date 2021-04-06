dcebec2ad01127b34233fa2a77a66d228fef7154
Itamar Axelrad <iaxelrad@gmail.com>
2021-04-07 00:14:17 +0300

    created useContent custom hook to retrieve data.

--- src/pages/browse.js
+++ src/pages/browse.js
@@ -1,5 +1,13 @@
 import React from 'react';
-
+import { useContent } from '../hooks';
 export default function Browse() {
+  // we need the series and the Films
+  const { series } = useContent('series');
+  const { films } = useContent('films');
+  console.log(series);
+  console.log(films);
+  //we need slides
+  //pass it to the browse container
+
   return <div>Hello from Browse</div>;
 }
