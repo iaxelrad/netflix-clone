cd861dbacc9aa528eed8f3c198dc37efaf3bba2d
Itamar Axelrad <iaxelrad@gmail.com>
2021-04-07 07:47:43 +0300

    added simple initial browse container.

---
+++ src/containers/browse.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { Browse } from '../components';
+
+export function BrowseContainer({ children }) {
+  return <Browse>{children}</Browse>;
+}
--- src/pages/browse.js
+++ src/pages/browse.js
@@ -1,13 +1,13 @@
 import React from 'react';
+import BrowseContainer from '../components/browse';
 import { useContent } from '../hooks';
+import selectionFilter from '../utils/selection-filter';
+
 export default function Browse() {
-  // we need the series and the Films
   const { series } = useContent('series');
   const { films } = useContent('films');
-  console.log(series);
-  console.log(films);
-  //we need slides
-  //pass it to the browse container
 
-  return <div>Hello from Browse</div>;
+  const slides = selectionFilter({ films, series });
+
+  return <BrowseContainer slides={slides} />;
 }
