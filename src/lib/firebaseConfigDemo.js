b6bfd090d3e5eaf6059274ac5edd3e3a32d8dcb8
Itamar Axelrad <iaxelrad@gmail.com>
2021-04-09 09:11:31 +0300

    template for actual firebase config values.

---
+++ src/lib/firebaseConfigDemo.js
@@ -0,0 +1,10 @@
+//Not actual working values - Replace with your own firebase credentials.
+
+export const firebaseConfig = {
+  apiKey: 'AIzaSyB34RwvbZFfFlsOtfDbEc-rN_t2B-sty-b',
+  authDomain: 'name-of-app.firebaseapp.com',
+  projectId: 'name-of-app',
+  storageBucket: 'name-of-app.appspot.com',
+  messagingSenderId: 'messagingSenderId',
+  appId: '1:messagingSenderId:web:randomly-generated-from-firebase',
+};
