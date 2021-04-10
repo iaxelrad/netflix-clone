38d60040b8a41f7a9784bb6d28a6793893bde1c4
Itamar Axelrad <iaxelrad@gmail.com>
2021-04-10 11:39:39 +0300

    sign-up test added

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -44,7 +44,7 @@ export default function SignUp() {
       <HeaderContainer>
         <Form>
           <Form.Title>Sign Up</Form.Title>
-          {error && <Form.Error>{error}</Form.Error>}
+          {error && <Form.Error data-testid="error">{error}</Form.Error>}
           <Form.Base onSubmit={handleSignUp} method="POST">
             <Form.Input
               placeholder="First name"
@@ -63,7 +63,11 @@ export default function SignUp() {
               value={password}
               onChange={({ target }) => setPassword(target.value)}
             />
-            <Form.Submit disabled={isInvalid} type="submit">
+            <Form.Submit
+              data-testid="sign-up"
+              disabled={isInvalid}
+              type="submit"
+            >
               Sign Up
             </Form.Submit>
           </Form.Base>
